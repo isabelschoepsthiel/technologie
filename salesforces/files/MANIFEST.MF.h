d8f370e40e8a806c6e4ed89a555afd80a52d24dd
Isabel Schöps Thiel <isabelschoepsthiel@gmail.com>
2025-08-08 20:48:49 +0200

    copyright

---
+++ salesforces/files/MANIFEST.MF.h
@@ -0,0 +1,17 @@
+Manifest-Version: 1.0
+
+Name: dataloader-64.0.2.jar
+SHA-256-Digest: H2dzEsYa3r6Eobjz5G8OxlXvpsO10MWoyZR5NZzwLFk=
+
+Name: install.bat
+SHA-256-Digest: vRgYYLWfYIRSLc0EvH3dk88EEKTEXwg+4c1lHWmlvpY=
+
+Name: install.command
+SHA-256-Digest: vRgYYLWfYIRSLc0EvH3dk88EEKTEXwg+4c1lHWmlvpY=
+
+Name: util/util.bat
+SHA-256-Digest: DCt8UzD4XSH6WxxPsk9i/aMSdgGQubia4IlyiSc19xQ=
+
+Name: util/util.sh
+SHA-256-Digest: odYcqHJgCqIgiU3G1kue085GlcWb1mm7Saf/o8OKmEo=
+
