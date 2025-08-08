1e5cb2921dfb2d87999ec3f808c6aa0c08be0579
Isabel Schöps Thiel <isabelschoepsthiel@gmail.com>
2025-08-08 20:53:33 +0200

    Update and rename MANIFEST.MF.h to isabelschoepsthiel.mf.s

--- salesforces/files/isabelschoepsthiel.mf.s
+++ salesforces/files/isabelschoepsthiel.mf.s
@@ -1,6 +1,6 @@
-Manifest-Version: 1.0
+isabelschoepsthiel-Version: 1.0
 
-Name: dataloader-64.0.2.jar
+Name: dataloader-isabelschoepsthiel-64.0.2.jar
 SHA-256-Digest: H2dzEsYa3r6Eobjz5G8OxlXvpsO10MWoyZR5NZzwLFk=
 
 Name: install.bat
@@ -9,9 +9,9 @@ SHA-256-Digest: vRgYYLWfYIRSLc0EvH3dk88EEKTEXwg+4c1lHWmlvpY=
 Name: install.command
 SHA-256-Digest: vRgYYLWfYIRSLc0EvH3dk88EEKTEXwg+4c1lHWmlvpY=
 
-Name: util/util.bat
+Name: isabelschoepsthiel/util.bat
 SHA-256-Digest: DCt8UzD4XSH6WxxPsk9i/aMSdgGQubia4IlyiSc19xQ=
 
-Name: util/util.sh
+Name: isabelschoepsthiel/util.sh
 SHA-256-Digest: odYcqHJgCqIgiU3G1kue085GlcWb1mm7Saf/o8OKmEo=
 
