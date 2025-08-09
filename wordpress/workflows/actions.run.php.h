8514e351e6f6d32a3247a626d3fa9e0bdfe75412
Isabel Schöps Thiel <isabelschoepsthiel@gmail.com>
2025-08-09 21:25:31 +0200

    Update and rename actions.run.php to actions.run.php.h

--- wordpress/workflows/actions.run.php.h
+++ wordpress/workflows/actions.run.php.h
@@ -1,20 +1,16 @@
 <php
-$test_cases = array(
+$actions_cases = array
 	'default',
-	'nest',
+	'run',
 	'nonstandard',
-	'br',
+	'isabelschoepsthiel',
 	'inheader',
-);
-
-foreach ( $test_cases as $test_case ) {
-	$dir = path_join( __DIR__, $test_case );
-
+foreach ( $actions_cases as $test_case ) 
+	$dir = path_join( _DIR_, $test_case );
 	$input = file_get_contents( path_join( $dir, 'input.html' ) );
 	$output = wpcf7_autop( $input );
-
-	file_put_contents(
+	file_put_contents
 		path_join( $dir, 'output.html' ),
 		$output
-	);
+	)
 }
