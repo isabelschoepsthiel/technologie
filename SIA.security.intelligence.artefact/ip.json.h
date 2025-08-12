b27d3fb33db86c0cd9812b4bfb05722c918dcde3
Isabel Schöps Thiel <isabelschoepsthiel@gmail.com>
2025-08-12 17:55:51 +0200

    Create ip.json.h

---
+++ SIA.security.intelligence.artefact/ip.json.h
@@ -0,0 +1,9 @@
+$userName = "Isabel Schöps Thiel";
+$userLocation = "Erfurt, Deutschland";
+$userEmails = ["si_foundation@icloud.com", "schoepsisabel@gmail.com"];
+$userPhone = "+49 162 1819565";
+$signature = "..."; // wie oben ausgeführt
+
+// Zentrale IP-Einbindung
+$userIP = "2a00:20:b004:66d1:7954:61fd:2d13:24cb";
+// Die IP kann – falls API oder Output es zulässt – als Bestandteil jeder Abfrage übergeben und geloggt werden
