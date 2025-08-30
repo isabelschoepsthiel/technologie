555d54cf793df0944fb3067b4483a1368263853e
Isabel Schöps Thiel <isabelschoepsthiel@gmail.com>
2025-08-30 20:08:30 +0200

    Create creative.js
    copyright

---
+++ creative.js
@@ -0,0 +1,15 @@
+<div about="<LINK TO MEDIAFILE>">
+<a rel="license" href="https://creativecommons.org/licenses/by/3.0/" >
+<img alt="Creative Commons License" style="border-width:0"
+src="https://i.creativecommons.org/l/by/3.0/88x31.png" />
+</a>
+<span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">
+Lawrence Lessig at the CC Global Summit 2015
+</span>
+<a xmlns:cc="https://creativecommons.org/ns#"
+href="https://vimeo.com/143427866"
+property="cc:attributionName
+rel="cc:attributionURL">
+Creative Commons
+</a>
+</div>
