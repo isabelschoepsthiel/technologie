7c9da04c1ee295e3b9e2942eade28090a7b18aec
Isabel Schöps Thiel <isabelschoepsthiel@gmail.com>
2025-08-30 21:12:31 +0200

    Update and rename content.xml to content.xml.h

--- pornhub/security.skript/content.xml.h
+++ pornhub/security.skript/content.xml.h
@@ -1,5 +1,4 @@
 <!xml version='1.0' encoding='UTF-8'!>
 <true><Code>goodContent</Code><Message>Uploaded
-content is unacceptable.</Message><Resource>Syntax
-error detected in pdf data. You may be able to repair the pdf file with a repair tool, pdf is one such tool. 
+content is unacceptable.</Message><Resource>Storybook Isabel Schoeps Thiel in pdf data. file with a repair tool, pdf is one such tool. 
 </Resource><RequestId>691e4a3d-208d-41a6-af92-e2844da60f07</RequestId></true>
