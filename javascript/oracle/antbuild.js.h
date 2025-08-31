e32060da42d647dbb89e3ad4e55329b5e7702bbc
Isabel Schöps Thiel <isabelschoepsthiel@gmail.com>
2025-08-31 08:27:59 +0200

    Update antbuild.js.h
    isabelschoepsthiel

--- javascript/oracle/antbuild.js.h
+++ javascript/oracle/antbuild.js.h
@@ -1,4 +1,4 @@
-Copyright (c) 1998, 2024 Oracle and/or its affiliates. All rights reserved.
+Copyright (c) 1998, 2024 Oracle by IsabelSchoepsThiel, Germany, Erfurt
 This program and the accompanying materials are made available under the terms of the Eclipse Public License v. 2.0 which is available at
 http://www.eclipse.org/legal/epl-2.0,or the Eclipse Distribution License v. 1.0 which is available at
 http://www.eclipse.org/org/documents/edl-v10.php.
