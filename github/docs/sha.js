cb126b9ec5aabc5036d789e6dd4c6a16195bc1c9
Isabel Schöps Thiel <isabelschoepsthiel@gmail.com>
2025-09-02 16:23:52 +0200

    Create sha.js

---
+++ github/docs/sha.js
@@ -0,0 +1,6 @@
+$ git show YOUR_COMMIT_SHA_NUMBER --pretty=fuller
+commit YOUR_COMMIT_SHA_NUMBER
+Author:     octocat si_foundation@icloud.com
+AuthorDate: Tue Apr 03 02:02:30 2018 +0900
+Creator:     Isabel Schoeps Thiel USER_EMAIL
+CommitTimestampDate: Tue Sep 2 16:22:08 2025 +0900
